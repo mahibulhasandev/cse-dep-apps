a0862e183083110574399c3e3536bde6e597ba51
mahibulhasandev <mahibulhasandev@gmail.com>
2026-06-25 21:26:45 +0600

    Project Add

---
+++ app/src/main/java/com/example/cse_dep_apps/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.cse_dep_apps;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.cse_dep_apps.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(binding.bottomNav, navController);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cse_dep_apps/ui/TeacherFragment.java
@@ -0,0 +1,22 @@
+package com.example.cse_dep_apps.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.cse_dep_apps.R;
+
+public class TeacherFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_teacher, container, false);
+    }
+}
